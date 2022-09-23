9df3d8592acb6dcd34b2176576a0a45fba41698f
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-09-23 19:22:30 -0300

    criado classe e adicionado props na classe Pessoa

---
+++ ProjectOngAnimais/ProjectOngAnimais/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOngAnimais
+{
+    internal class Pessoa
+    {
+        public String CPF { get; set; }
+        public String Nome { get; set; }
+        public Char Sexo { get; set; }
+        public String telefone { get; set; }
+        public Endereco End { get; set; }
+        public Char Ativa { get; set; }
+
+        public Pessoa()
+        {
+            Ativa = 'A';
+        }
+
+    }
+}
